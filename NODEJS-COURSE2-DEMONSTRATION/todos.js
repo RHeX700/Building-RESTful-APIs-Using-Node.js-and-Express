b2e3f4e1c1304f8abdc2cf1968746daf6a8487aa
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-04-30 12:04:36 +0100

    learning to build a server with vanilla js

---
+++ NODEJS-COURSE2-DEMONSTRATION/todos.js
@@ -0,0 +1,28 @@
+todos = [
+    {
+        id : 1,
+        title : "Coding in Javascript",
+        description : "Writing functions in javascript",
+        completed : false
+    },
+    {
+        id : 2,
+        title : "Cooking supper",
+        description : "Cooking rice and chicken",
+        completed : false
+    },
+    {
+        id : 3,
+        title : "Taking a walk",
+        description : "Easy time at the park",
+        completed : false
+    },
+    {
+        id : 4,
+        title : "Watching netflix",
+        description : "Watching the new premiered series",
+        completed : false
+    },
+];
+
+module.exports = todos;
