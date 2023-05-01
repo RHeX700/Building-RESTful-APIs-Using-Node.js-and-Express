d8df2aa1b1ac47af70a2ba8e777ba0ee70bb0502
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-05-01 20:08:50 +0100

    productsService tests passed

--- product-api-using-vanilla-javascript/src/productsService.js
+++ product-api-using-vanilla-javascript/src/productsService.js
@@ -12,37 +12,51 @@ const getProductsById = (productId, done) => {
   let product = null
 
   // get a product by ID
-  if(lodash.findIndex(p => p.id == productId) === -1){
-    return done("Requested product doesn't exist..!")
-  }else{
-    product = lodash.find(productsList, p => p.id == productId);
-  
-
-    return done(null, JSON.stringify(product));
+  // if(lodash.findIndex(p => p.id == productId) === -1){
+  //   return done("Requested product doesn't exist..!")
+  // }
+  product = lodash.find(productsList, p => p.id == productId);
+  if(!product){
+    done("Requested product doesn't exist..!");
   }
+
+  return done(null, JSON.stringify(product));
 }
 
 const saveProduct = (newProduct, done) => {
  // save a product
-  productsList.push(newProduct);
-  return done(null, JSON.stringify(productsList));
+  let product = lodash.find(productsList, p => p.id == newProduct.id);
+  if(!product){
+    productsList.push(newProduct);
+    return done(null, JSON.stringify(productsList));    
+  }
+  return done("Product already exists..!")
+
 }
 
 const updateProduct = (productId, updateData, done) => {
   let updatedProductList = null;
   // update the product list
-  let productIndex = lodash.findIndex(getProductsById(productId));
-  productsList[productIndex].name = updateData.name;
-  productsList[productIndex].description = updateData.description;
-  productsList[productIndex].price = updateData.price;
-  productsList[productIndex].quantity = updateData.quantity;
+  let product = lodash.find(productsList, p => p.id == productId);
+  if(product){
+    let productIndex = lodash.findIndex(productsList, p => p.id == productId);
+    productsList[productIndex].name = updateData.name;
+    productsList[productIndex].description = updateData.description;
+    productsList[productIndex].price = updateData.price;
+    productsList[productIndex].quantity = updateData.quantity;
+  }else{
+    done("Requested product doesn't exist..!");
+  }
 
-  done(null, JSON.stringify(productList));
+  done(null, JSON.stringify(productsList));
 }
 
 const deleteProduct = (productId, done) => {
   // delete a product    
-  let productIndex = lodash.findIndex(getProductsById(productId));
+  let productIndex = lodash.findIndex(productsList, p => p.id == productId);
+  if(productIndex == -1){
+    return done("Requested product doesn't exist..!")
+  }
   productsList.splice(productIndex, 1)
   done(null, JSON.stringify(productsList));
 }
